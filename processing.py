734fb49cc7c099d9d3e19589fdbf327614618f53
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-04-17 20:29:31 +0500

    + исключение, + вывод сообщений в цикле
    1. Добавлен вывод если квадрат числа больше или равен 20 и меньше или равен 50
    2. Добавлен вывод если квадрат числа меньше 20
    3. Добавлена обработка исключения ValueError

--- processing.py
+++ processing.py
@@ -1,8 +1,14 @@
-string = 'Это просто строка'
+string = ''
 boolean = True
 floating = 10.0
 second_floating = 15.0
 def processing(a, b, c):
+    try:
+        if len(a) < 1:
+            raise ValueError
+    except ValueError:
+        print('Ошибка: строка пустая')
+    
     number = 0
     while number <= c:
         if number == 4.0:
@@ -12,11 +18,19 @@ def processing(a, b, c):
         elif number == 9.0:
             print('отсчет окончен на 9')
             break
-        if (number ** 2) > 50:
+        if (number ** 2) < 20:
+            print("Квадрат числа {number} меньше 20")
+            number += 1.0
+        elif (number ** 2) >= 20 and (number ** 2) <= 50:
+            print("Квадрат числа {number} больше или равен 20 и меньше или равен 50 ")
+            number += 1.0
+        elif (number ** 2) > 50:
             print("Квадрат числа {number} больше 50")
             number += 1.0
+        
         else:
 	        number += 1.0
+        
 
 processing(string, boolean, floating)
 processing(string, boolean, second_floating)

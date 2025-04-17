758e169e939d1f45c21b0e90f210478f1624b092
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-04-17 20:08:40 +0500

    Доработка цикла в функции
    1. Добавление условий при которых будет пропуск итерации на 4 и окончание перебора на 9, и в случае если квадрат числа больше 50
    2.  Второй вызов функции с другими значениями

--- processing.py
+++ processing.py
@@ -1,12 +1,22 @@
 string = 'Это просто строка'
 boolean = True
 floating = 10.0
-
-
-def processing(a, b, c):  
-    x = 0
-    while x <= c:
-        print(x ** 2)
-        x += 1.0
+second_floating = 15.0
+def processing(a, b, c):
+    number = 0
+    while number <= c:
+        if number == 4.0:
+            print('пропуск итерации на 4')
+            number += 1.0
+            continue
+        elif number == 9.0:
+            print('отсчет окончен на 9')
+            break
+        if (number ** 2) > 50:
+            print("Квадрат числа {number} больше 50")
+            number += 1.0
+        else:
+	        number += 1.0
 
 processing(string, boolean, floating)
+processing(string, boolean, second_floating)

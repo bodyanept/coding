da3e74ad74152fa767e3b792c0c42b1187a9677d
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-04-29 19:26:24 +0500

    Новая функция создания файла
    1. Создание нового файла
    2. Функция вычисления факториала
    3. добавление результата функции факториала в новый файл

--- files.py
+++ files.py
@@ -5,3 +5,16 @@ def open_file():
             print('Файл открыт!')
     except FileNotFoundError:
         print('Файл не найден')
+
+
+def new_file():
+    with open('factorial.txt', 'w') as factorial:
+        def f(n):
+            if n == 1: # Условие выхода
+                return 1
+            else:
+                return n * f(n-1) 
+        factorial.write(str(f(11)))
+        print(f(11))
+        
+new_file()

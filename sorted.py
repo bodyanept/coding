8c8f0b622901f14ca6b7a9eb384e72a586becab2
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-05-06 20:28:20 +0500

    подсчет времени выполнения каждой функции
    1. Рандомный список, который сгенерирован с помощью модуля random
    2. Подсчет времени выполнения каждой функции с помощью модуля time

--- sorted.py
+++ sorted.py
@@ -1,5 +1,13 @@
-x = [5, 9, 8, 1, 3, 11, 6]
+import random
 
+import time
+
+
+
+x = [random.randint(0, 100000) for _ in range(10000)]
+
+
+start_time = time.time()
 def selection_sort(x):
     n = len(x)
     for i in range(n):
@@ -9,9 +17,13 @@ def selection_sort(x):
                 min = j
         x[i], x[min] = x[min], x[i]    
     return x
-print(selection_sort(x))
+end_time = time.time()
+elapsed_time = end_time - start_time
 
-
+# print(f'До: {x}')
+# print(f'После сортровки методом selection: {selection_sort(x)}')
+print(f"Функция selection_sort выполнилась за {elapsed_time} секунд(ы)")
+start_time = time.time()
 def buble_sort(x):
     n = len(x)
     for i in range(n):
@@ -19,14 +31,22 @@ def buble_sort(x):
             if x[j] > x[j+1]:
                 x[j], x[j + 1] = x[j + 1], x[j]    
     return x
-buble_sorted = buble_sort(x)
+end_time = time.time()
+elapsed_time = end_time - start_time
+
     
-print(f'До: {x}')
-print(f'После сортровки методом bubble: {buble_sorted}')
+# print(f'До: {x}')
+# print(f'После сортровки методом bubble: {buble_sort(x)}')
+print(f"Функция buble_sort выполнилась за {elapsed_time} секунд(ы)")
 
-
+start_time = time.time()
 def timsort(x):
     return sorted(x)
+end_time = time.time()
+elapsed_time = end_time - start_time
     
-print(f'До: {x}')
-print(f'После сортровки методом timsort: {timsort(x)}')
+# print(f'До: {x}')
+# print(f'После сортровки методом timsort: {timsort(x)}')
+print(f"Функция timsort выполнилась за {elapsed_time} секунд(ы)")
+
+

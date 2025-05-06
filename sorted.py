0d9033c026e765a5d8e718fd535161b808869a3d
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-05-06 20:10:00 +0500

    еще функция
    функция сортировки методом bubble

--- sorted.py
+++ sorted.py
@@ -10,3 +10,16 @@ def selection_sort(x):
         x[i], x[min] = x[min], x[i]    
     return x
 print(selection_sort(x))
+
+
+def buble_sort(x):
+    n = len(x)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if x[j] > x[j+1]:
+                x[j], x[j + 1] = x[j + 1], x[j]    
+    return x
+buble_sorted = buble_sort(x)
+    
+print(f'До: {x}')
+print(f'После сортровки методом bubble: {buble_sorted}')

b4a1144462d3d769f750c56f3b5034d92c93e9b0
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-05-06 19:49:29 +0500

    Ручная функция сортировки
    сортировка методом selection sort

--- sorted.py
+++ sorted.py
@@ -1,6 +1,12 @@
 x = [5, 9, 8, 1, 3, 11, 6]
 
-def sorted_funk(x):
-    return sorted(x)
-    
-print(sorted_funk(x))
+def selection_sort(x):
+    n = len(x)
+    for i in range(n):
+        min = i
+        for j in range(i+1, n):
+            if x[j] < x[min]:
+                min = j
+        x[i], x[min] = x[min], x[i]    
+    return x
+print(selection_sort(x))

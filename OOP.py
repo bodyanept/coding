0ada3589259b0ac8b24b2eae2984654227246d99
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-05-15 20:51:45 +0500

    Банк акк
    1. новый класс банковского аккаунта с номером счета и балансом
    2. метод добавления приватности
    3. метод для взноса депозита с проверкой на корректность ввода
    4. метод для снятия монеты с проверкой на наличие необходимой суммы

--- OOP.py
+++ OOP.py
@@ -23,3 +23,37 @@ book3.add_book('Война и мир','Лев Толстой',1867 )
 book1.get_info()
 book2.get_info()
 book3.get_info()
+
+
+
+class BankAccount:
+    number = None
+    balance = None
+    
+    def __init__(self,number,balance):
+        self.__number = number
+        self.__balance = balance
+    
+    def deposit(self,amount):
+        if amount > 0:
+            self.__balance += amount
+        else:
+            print('Вы не можете положить меньше 0 рублей)')
+            
+    def withdraw(self,amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            print('на вашем счете не достаточно средств')
+    def get_balance(self):
+        print (self.__balance)
+    def get_number(self):
+        print (self.__number)
+        
+
+        
+account = BankAccount(123456,1000)
+account.deposit(500)
+
+account.get_balance()
+account.get_number()

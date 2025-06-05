426921cd740d6b742ca70f00df8c1f60518afad4
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-06-05 19:12:58 +0500

    Update education.py
    атрибуты класса студент стали приватными, добавлены геттеры этих атрибутов

--- education.py
+++ education.py
@@ -1,19 +1,31 @@
 class Student:
     def __init__(self, name, student_id):
-        self.name = name
-        self.student_id = student_id
-        self.grades = []
-    def add_grade(self, grade):
-    self.grades.append(grade)
+        self.__name = name
+        self.__student_id = student_id
+        self.__grades = []
+    
+    def get_name():
+        return self.__name
+    
+    def get_id():
+        return self.__get_id
+    
+    def get_grades():
+        return self.__get_grades
         
-    def get_average(self):
-        avg = sum(self.grades) / len(self.grades)
-        print(f'Средний балл студента {self.name} составляет : {avg}')
+        
     def display_info(self):
         print(f'''
-        Имя:{self.name}
-        ID:{self.student_id}
+        Имя:{self.__name}
+        ID:{self.__student_id}
         ''')
+    def add_grade(self, grade):
+        self.__grades.append(grade)
+        
+    def get_average(self):
+        avg = sum(self.__grades) / len(self.__grades)
+        print(f'Средний балл студента {self.__name} составляет : {avg}')
+        
 class Group:
     def __init__(self):
         self.students = []
@@ -21,11 +33,11 @@ class Group:
     def add_student(self,student):
         self.students.append(student)
         
+
     def show_students(self):
         if len(self.students) < 1:
             print('Группа пуста')
         else:
             for student in self.students:
                 student.display_info()
-        
         

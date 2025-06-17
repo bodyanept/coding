fd8db45ba31848c123b076f559b2a797edb6c09d
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-06-17 21:03:41 +0500

    Update serializer.py

--- serializer.py
+++ serializer.py
@@ -7,30 +7,49 @@ class Book:
     def get_info(self):
         print(f'{self.name}, {self.author}, {self.year}')
         
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "author": self.author,
+            "year": self.year
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["name"], data["author"], data["year"])   
 
 class libary:
-    def add_book(self,book):
-        pass
+    def add_book(self, book):
+        self.save()
     
-    def del_book(self,book):
-        pass
+    def del_book(self, book):
+    
+        self.save()
     
     def display_info(self):
         pass
     
+    def save(self):
+        pass
+    def load(self):
+        pass
+    
+libary = libary()
+libary.load()
 while True:
-    input(f'''
+    act = input(f'''
     выберите действие с книгой:
     1 - добавить
     2 - удалить
     3 - вывести список книг
     ''')
-
+    
     if act == 1:
         libary.add_book()
     elif act == 2:
         libary.del_book()
     elif act == 3:
         libary.display_info()
+        
         
         

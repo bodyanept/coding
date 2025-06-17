488e19b5875deb9b2c25ed067e28f09de63101f6
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-06-17 20:48:33 +0500

    Create serializer.py
    класс libary
    методы:
    добавление книги
    удаление книги
    вывод всех книг на экран

---
+++ serializer.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+
+    def get_info(self):
+        print(f'{self.name}, {self.author}, {self.year}')
+        
+
+class libary:
+    def add_book(self,book):
+        pass
+    
+    def del_book(self,book):
+        pass
+    
+    def display_info(self):
+        pass
+    

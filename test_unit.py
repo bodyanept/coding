29a6a955001f761ba6f05292043cfdbe77775b39
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-07-08 19:29:04 +0500

    класс юниттеста
    добавился класс юниттеста где проходит проверка методов класса тест

--- test_unit.py
+++ test_unit.py
@@ -1,9 +1,11 @@
+import unittest
+
 class Test:
     def factorial(self,chis):
         if chis == 0:
             return 1
         else:
-            return chis * factorial(n-1)
+            return chis * self.factorial(chis-1)
 
 
     def stepan(self,chis , step):
@@ -12,7 +14,15 @@ class Test:
 
     def formula(self,a,b):
         return (a ** 2) + (b ** 2)
+
 test = Test()
 
+class Unit_test(unittest.TestCase):
+    def test_fact(self):
+        self.assertEqual(test.factorial(5),720)
+    def test_stepan(self):
+        self.assertEqual(test.stepan(6,2),36)
+    def test_fact(self):
+        self.assertEqual(test.formula(2,3),13)
 
-print(test.stepan(6,2))
+unittest.main()

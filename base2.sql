2454a330635204058ed498ceb3d31902076edfa3
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-08-12 20:02:16 +0500

    Update base2.sql

--- base2.sql
+++ base2.sql
@@ -4,3 +4,10 @@ CREATE TABLE authors (
   group_id INT NOT NULL,
   PRIMARY KEY(id)
 );
+
+
+CREATE TABLE groups (
+  id INT NOT NULL AUTO_INCREMENT,
+  group_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY(id)
+);

d72f27a30d35ac6670416e964cd60edf070a1585
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-08-12 20:03:55 +0500

    Update base2.sql

--- base2.sql
+++ base2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE authors (
+CREATE TABLE students  (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   group_id INT NOT NULL,
@@ -11,3 +11,8 @@ CREATE TABLE groups (
   group_name VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 );
+
+
+ALTER TABLE students
+ADD CONSTRAINT fk_students_groups
+FOREIGN KEY (group_id) REFERENCES groups(id);

f661488c78f870985fe04d824e1212b9fa111021
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-08-12 20:20:50 +0500

    Update base2.sql

--- base2.sql
+++ base2.sql
@@ -1,30 +1,39 @@
-CREATE TABLE students  (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  group_id INT NOT NULL,
-  PRIMARY KEY(id)
-);
-
 
 CREATE TABLE groups (
-  id INT NOT NULL AUTO_INCREMENT,
-  group_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    group_name VARCHAR(50) NOT NULL
 );
 
 
-ALTER TABLE students
-ADD CONSTRAINT fk_students_groups
-FOREIGN KEY (group_id) REFERENCES groups(id);
+
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100) NOT NULL,
+    group_id INT NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+);
+
 
 INSERT INTO groups (group_name) VALUES
 ('Художественная литература'),
 ('Научная фантастика'),
 ('Техническая литература');
 
+
 INSERT INTO students (name, group_id) VALUES
 ('Иван Петров', 1),
 ('Мария Сидорова', 2),
-('Алексей Иванов', 3), 
+('Алексей Иванов', 3),
 ('Елена Кузнецова', 1),
-('Дмитрий Смирнов', 2);  
+('Дмитрий Смирнов', 2);
+
+
+SELECT 
+    s.id AS student_id,
+    s.name AS student_name,
+    g.id AS group_id,
+    g.group_name
+FROM 
+    students s
+JOIN 
+    groups g ON s.group_id = g.id;
